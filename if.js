bd0f4865a7b59adadf6e350a2df2f45d477b1774
kent winter <kgw700@gmail.com>
2018-01-14 18:51:17 +0000

    automatically backed up by learn

--- if.js
+++ if.js
@@ -5,6 +5,14 @@ if (pokemon == "Pikachu") {
 }
 
 function greaterThanTen(num) {
-  if (num > 10)
+  if (num === 10) {
+    return (num);
+  } else if (num <=10) {
+    return "sorry your number is not 10";
+  } else if (num>10) {
+    return "greater than ten";
+  
+  }
+  
 }
 
